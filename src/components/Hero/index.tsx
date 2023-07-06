758006e0b58c803f8a77356ce028dc856db53695
Luís Fernando Dandolini Duarte <luisfernandodandoliniduarte@gmail.com>
2023-07-06 14:58:20 -0300

    feat: create component hero

---
+++ src/components/Hero/index.tsx
@@ -0,0 +1,25 @@
+import './style.css';
+import HeroImage from '../../assets/Hero.png';
+import Esclamacao from '../../assets/Exclamação.png';
+
+export default function Hero() {
+  return(
+    <section className='container_hero'>
+      <div className='container_content'>
+        <h1 className='title'>
+          Contectando <span>possibilidades,</span>
+          <br />
+          <div className='container_esclamacao'>
+            construindo <span>resultados</span>
+            <img src={Esclamacao} alt="Esclamação" />
+          </div>
+        </h1>
+        <div className='container_button'>
+          <button>Entre em contato</button>
+        </div>
+      </div>
+
+      <img src={HeroImage} alt="Hero imagem" className='hero_image' />
+    </section>
+  )
+}
