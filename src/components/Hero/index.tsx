a46e1bf29aa0870b85d0e545f688fdaa4a09771b
Luís Fernando Dandolini Duarte <luisfernandodandoliniduarte@gmail.com>
2023-07-07 14:20:14 -0300

    feat: create mobile hero

--- src/components/Hero/index.tsx
+++ src/components/Hero/index.tsx
@@ -1,8 +1,17 @@
 import './styles.css';
 import HeroImage from '../../assets/Hero.png';
 import Esclamacao from '../../assets/Exclamação.png';
+import { useMediaQuery } from 'react-responsive';
+import { useState, useEffect } from 'react';
 
 export default function Hero() {
+  const [isMobile, setIsMobile] = useState(false);
+  const isMobileQuery = useMediaQuery({ query: `(max-width: 767px)` });
+
+  useEffect(() => {
+    setIsMobile(isMobileQuery);
+  }, [isMobileQuery]);
+
   return(
     <section className='container_hero'>
       <div className='container_content'>
@@ -10,16 +19,31 @@ export default function Hero() {
           Contectando <span>possibilidades,</span>
           <br />
           <div className='container_esclamacao'>
-            construindo <span>resultados</span>
-            <img src={Esclamacao} alt="Esclamação" />
+            construindo <span>resultados <img src={Esclamacao} alt="Esclamação" /></span>
           </div>
         </h1>
-        <div className='container_button'>
+        {
+          isMobile ? (
+            <div className='img_mobile'>
+              <img src={HeroImage} alt="Hero imagem" className='hero_image' />
+            </div>
+          ):(
+            <></>
+          )
+        }
+        
+        <div className='container_button_hero'>
           <button>Entre em contato</button>
         </div>
       </div>
 
-      <img src={HeroImage} alt="Hero imagem" className='hero_image' />
+      {
+        isMobile ? (
+          <></>
+        ):(
+          <img src={HeroImage} alt="Hero imagem" className='hero_image' />
+        )
+      }
     </section>
   )
 }
