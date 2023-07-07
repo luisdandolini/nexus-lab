d02af5cdd51a31161392b0689440cb05efd8b0ca
Luís Fernando Dandolini Duarte <luisfernandodandoliniduarte@gmail.com>
2023-07-07 14:40:22 -0300

    feat: create portfolio mobile

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -14,8 +14,8 @@ export default function Home() {
       <Hero />
       <More />
       <About />
-      {/* <Portfolio />
-      <Process />
+      <Portfolio />
+      {/* <Process />
       <Products />
       <Footer /> */}
     </>
